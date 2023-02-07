2d0377e17f5e5827fb00687f15203d4d8dce4083
iyacino <yacinetaamourt@gmail.com>
2023-02-07 23:39:15 +0100

    adding the hamburger menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+function navToggle(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('hidden');
+};
+
+btn.addEventListener('click', navToggle)
